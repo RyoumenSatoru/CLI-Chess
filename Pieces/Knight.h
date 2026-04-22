c523895d1f392b8bebe675be73b93582d4a5371b
C_Has_Yagami <srudgmaer11@gmail.com>
2026-04-22 18:28:41 +0530

    Eigth : Knight Movement

--- Pieces/Knight.h
+++ Pieces/Knight.h
@@ -12,7 +12,20 @@ public:
     }
 
     bool Is_Valid_Move(std::pair<int, int> Destination, Piece* Board[8][8]) override{
-        return true;
+        int Src_Row = Current_Position.first;
+        int Src_Col = Current_Position.second;
+
+        int Dest_Row = Destination.first;
+        int Dest_Col = Destination.second;
+
+        int Row_Diff = abs(Src_Row - Dest_Row);
+        int Col_Diff = abs(Src_Col - Dest_Col);
+
+        if((Row_Diff == 1 && Col_Diff == 2) || (Row_Diff == 2 && Col_Diff == 1)){
+            return true;
+        }
+
+        return false;
     }
 };
 
