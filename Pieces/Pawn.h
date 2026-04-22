8cec24cc1e546f45ab9e1bd75988f30ad650abbb
C_Has_Yagami <srudgmaer11@gmail.com>
2026-04-23 02:47:07 +0530

    Tenth : Pawn Movement

--- Pieces/Pawn.h
+++ Pieces/Pawn.h
@@ -12,7 +12,47 @@ public:
     }
 
     bool Is_Valid_Move(std::pair<int, int> Destination, Piece* Board[8][8]) override{
-        return true;
+        int C = Get_Color();
+        int Direction = (C == 'W') ? 1 : -1;
+
+        int Src_Row = Current_Position.first;
+        int Src_Col = Current_Position.second;
+
+        int Dest_Row = Destination.first;
+        int Dest_Col = Destination.second;
+
+        if((Dest_Row == Src_Row + Direction) && (Dest_Col == Src_Col)){
+            if(Board[Dest_Row][Dest_Col] != nullptr){
+                return false;
+            }
+            return true;
+        }
+
+        if(Dest_Row == Src_Row + (2*Direction) && (Dest_Col == Src_Col)){
+            if(Board[Src_Row + Direction][Src_Col] != nullptr || Board[Dest_Row][Dest_Col] != nullptr){
+                return false;
+            }
+
+            if((Src_Row == 1 && Get_Color() == 'W') || (Src_Row == 6 && Get_Color() == 'B')){
+                return true;
+            }
+        }
+
+        if((Dest_Row == Src_Row + Direction) && (abs(Dest_Col - Src_Col) == 1)){
+            if(Board[Dest_Row][Dest_Col] == nullptr){
+                return false;
+            }
+
+            if(Board[Dest_Row][Dest_Col]->Get_Color() == Board[Src_Row][Src_Col]->Get_Color()){
+                return false;
+            }
+
+            if(Board[Dest_Row][Dest_Col]->Get_Color() != Board[Src_Row][Src_Col]->Get_Color()){
+                return true;
+            }
+        }
+
+        return false;
     }
 };
 
